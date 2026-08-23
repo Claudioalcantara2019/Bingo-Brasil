c90bd46fea4698a9455dcb89ab060207c1b3e196
Claudio Alcantara <alcantaraclaudio123@gmail.com>
2026-08-23 03:37:01 -0300

    Bingo Brasil - componentes dos anfitrioes

---
+++ src/components/hosts/BiancaHost.tsx
@@ -0,0 +1,25 @@
+import { Image, StyleSheet, View } from 'react-native';
+
+export default function BiancaHost() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('@/assets/images/REFERENCIA-MESTRE-BIANCA-BOB.png')}
+        style={styles.image}
+        resizeMode="contain"
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  image: {
+    width: 180,
+    height: 180,
+  },
+});
